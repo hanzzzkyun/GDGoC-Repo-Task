cfed7f02f7fc8482af4b15008e23d39f3f7b0660
Hanzzz <reyhanfahreza3047@gmail.com>
2025-10-25 02:04:02 +0800

    first commit

---
+++ AngkaBom.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(){
+    int min = 1, max = 100, tebak = 0, loop = 1;
+    srand(time(NULL));
+    int bom = rand() % 100 + 1;
+    for (;;){
+
+        printf ("Tebak angka bom dengan range %d - %d: ", min, max);
+        scanf ("%d", &tebak);
+        if (tebak < min || tebak > max){
+            printf ("Tebakan tidak masuk range\n\n");
+        } else if (tebak < bom){
+            printf ("\nAngka Bom lebih besar\n\n");
+            min = tebak;
+            loop++;
+        } else if (tebak > bom){
+            printf ("\nAngka Bom lebih kecil\n\n");
+            max = tebak;
+            loop++;
+        } else if (tebak == bom) {
+            printf ("\nTebakan anda benar!!!\n");
+            printf ("Anda menebak sebanyak %d kali", loop);
+            break;
+        }
+    }
+    return 0;
+}
